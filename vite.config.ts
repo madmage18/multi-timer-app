24c40305c5b3fc91cad53c9e56ffac7e20e6370a
Madelyn Magee <mad.mage18@gmail.com>
2024-10-18 11:04:38 -0400

    Update mp3 import. update some devdependencies to dependencies. add server config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: Number(process.env.PORT) || 5173,
+    host: "0.0.0.0", // Ensure the server is accessible externally
+    strictPort: true, // Fail if the port is already in use
+  },
 })
